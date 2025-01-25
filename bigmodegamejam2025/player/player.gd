e686c5e761b15f0e7b05a54ed2023ac6f55f7261
pobertt <79865756+pobertt@users.noreply.github.com>
2025-01-25 17:57:50 +0000

    bare bones player and world

---
+++ bigmodegamejam2025/player/player.gd
@@ -0,0 +1,44 @@
+extends CharacterBody3D
+
+@onready var camera: Camera3D = $head/camera
+
+const SPEED = 5.0
+const JUMP_VELOCITY = 4.5
+
+
+func _ready():
+	# Capturing the mouse to the screen.
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	
+func _unhandled_input(event: InputEvent) -> void:
+	# Camera Rotation.
+	if event is InputEventMouseMotion:
+		rotate_y(-event.relative.x * .005)
+		camera.rotate_x(-event.relative.y * .005)
+		camera.rotation.x = clamp(camera.rotation.x, -PI/2, PI/2)
+
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("quit"):
+		get_tree().quit()
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = JUMP_VELOCITY
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var input_dir := Input.get_vector("left", "right", "forward", "back")
+	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if direction:
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		velocity.z = move_toward(velocity.z, 0, SPEED)
+
+	move_and_slide()

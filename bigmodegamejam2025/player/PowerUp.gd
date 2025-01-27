d91c488efbbfd1304ac2bea811d832e912e549b3
Bryan <148474232+Brexittackle223@users.noreply.github.com>
2025-01-27 10:50:56 +0000

    Create PowerUp.gd

---
+++ bigmodegamejam2025/player/PowerUp.gd
@@ -0,0 +1,22 @@
+extends Node
+
+# POWER UP TYPES GET DEFFINED HERE
+enum PowerUpType {
+	SMOKING,
+	DRINKING,
+	SNUSING,
+	CAR_KEYS
+}
+
+@export var power_up_type : PowerUpType
+
+signal collected(power_up_type)
+
+func _ready():
+	connect("body_entered", Callable(self, "_on_body_entered"))
+
+# FUNCTION FOR WHEN THE PLAYER ENTERS THE POWER UP AREA
+func _on_body_entered(body):
+	if body.is_in_group("player"):  
+		emit_signal("collected", power_up_type)  
+		queue_free()  

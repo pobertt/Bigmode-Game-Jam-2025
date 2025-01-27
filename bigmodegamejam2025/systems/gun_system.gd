d951275e14e7c85e0716c0beb4c1cc08ef733aa6
pobertt <79865756+pobertt@users.noreply.github.com>
2025-01-27 18:12:56 +0000

    Update gun_system.gd

--- bigmodegamejam2025/systems/gun_system.gd
+++ bigmodegamejam2025/systems/gun_system.gd
@@ -7,3 +7,9 @@ var current_gun : Gun
 
 func shoot():
 	current_gun = parent.current_gun
+	
+	if parent.can_shoot == true and parent.current_bullets > 0:
+		var valid_bullets : Array[Dictionary] = get_bullet_raycasts()
+		
+func get_bullet_raycasts():
+	pass

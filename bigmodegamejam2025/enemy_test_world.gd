eacf9c40ce4c3c5c8592983c077619e3d7b51afe
Skewdp <115475919+Skewdp@users.noreply.github.com>
2025-01-27 16:26:39 +0000

    Very simple AI
    Uses scene script to relay information

---
+++ bigmodegamejam2025/enemy_test_world.gd
@@ -0,0 +1,12 @@
+extends Node3D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+@onready var playerref = $Player
+
+# Provides players current world position and sends it to the enemy
+func _process(delta: float) -> void:
+	get_tree().call_group("enemy", "target_position", playerref.global_transform.origin)

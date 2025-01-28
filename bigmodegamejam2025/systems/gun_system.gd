582cc9748a42f174e28d0f900805e8c49d29dc1f
pobertt <79865756+pobertt@users.noreply.github.com>
2025-01-28 13:14:14 +0000

    Update gun_system.gd

--- bigmodegamejam2025/systems/gun_system.gd
+++ bigmodegamejam2025/systems/gun_system.gd
@@ -31,6 +31,7 @@ func shoot():
 		# if any bullets hits do all this stuff
 		if valid_bullets.is_empty() == false:
 			for b in valid_bullets:
+				print(current_gun.bullet_amt)
 				# Enemy Damage
 				if b.hit_target.is_in_group("enemy"): # Check if is enemy
 					b.hit_target.change_health(current_gun.damage * -1) # Can change this later but hurt/damage/kill enemy

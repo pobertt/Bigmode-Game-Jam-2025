58630d2e86dfbf95f3f9a98df3e1c4f655dc6191
Skewdp <115475919+Skewdp@users.noreply.github.com>
2025-01-29 18:55:26 +0000

    Proper player tracking and losing sight
    need to add roaming

--- bigmodegamejam2025/Enemy/enemy.gd
+++ bigmodegamejam2025/Enemy/enemy.gd
@@ -11,10 +11,6 @@ var PlayerRef
 const SPEED = 2.0
 const JUMP_VELOCITY = 4.5
 
-func _process(delta: float) -> void:
-	pass
-	#if AggroTimer.time_left > 0.0:
-	#	print("TimerActive")
 	
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -22,69 +18,87 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 	else:
 		velocity.y -= 2
-
+	
+	#check next target position
 	var next_location = navref.get_next_path_position()
+	#get current position
 	var current_location = global_transform.origin
 	#normalise for consistent speed
 	var new_velocity = (next_location - current_location).normalized() * SPEED
 	
+	#set velocity
 	velocity = new_velocity
 	
 	#smoothly adjust velocity
-	#velocity = velocity.move_toward(new_velocity, 0.25)
+	velocity = velocity.move_toward(new_velocity, 0.25)
 	
 	#apply motion
 	move_and_slide()
 
+#set next target position
 func target_position(target):
 	navref.target_position = target
 	
-
-
-
 #every time the timer elapses
 func _on_vision_timer_timeout():
 	#check overlapping bodies
 	var overlaps = VisionRef.get_overlapping_bodies()
+	var foundplayer = false
 	if overlaps.size() > 0:
 		for overlap in overlaps:
 			#confirm its the player
 			if overlap.name == "Player":
-				#update raycast with player location
-				var playerposition = overlap.global_transform.origin
-				RayCast.look_at(playerposition, Vector3.UP)
-				RayCast.force_raycast_update()
+				#set playerref for following after lostsight
 				PlayerRef = overlap
-				#if raycast is colliding
-				if RayCast.is_colliding():
-					#get collider
-					var collider = RayCast.get_collider()
-					#if colliding with player
-					if collider.name == "Player":
-						RayCast.debug_shape_custom_color = Color.RED
-						print("I see you")
-						#get looking at rotation so we can split it
-						var toplayerrotation = self.transform.looking_at(playerposition, Vector3.UP).basis.x
-						#set enemy rotation to look at player (only rotate on y)
-						rotation.y = toplayerrotation.z
-						#call target_position method on see player 
-						target_position(collider.global_transform.origin)
-					#if not colliding with player
-					else:
-						RayCast.debug_shape_custom_color = Color.GREEN
-						print("I don't see you")
-						
-							
-	if PlayerRef != null:
-		if PlayerRef.name == "Player":
-			if AggroTimer.time_left == 0.0:
-				AggroTimer.start()
-			elif AggroTimer.time_left > 0.0:
-				target_position(PlayerRef.global_transform.origin)
-				print("StillFollowing")
+				foundplayer = true
+				#update raycast with player location
+				_TrackPlayerRaycast()
+	#if player wasn't found but still exists in memory
+	if PlayerRef != null and foundplayer == false:
+		#if no aggro yet, start timer and save last position
+		if AggroTimer.time_left == 0.0:
+			AggroTimer.start()
+			_GoTowardsPlayer()
+		else:
+			pass
+		print("StillFollowing")
+	
 
+#manage enemy line of sight
+func _TrackPlayerRaycast():
+	RayCast.look_at(PlayerRef.global_transform.origin, Vector3.UP)
+	RayCast.force_raycast_update()
+	#check what the raycast is colliding with
+	if RayCast.is_colliding():
+		#get collider
+		var collider = RayCast.get_collider()
+		#if raycast can see player e.g. not hidden
+		if collider.name == "Player":
+			RayCast.debug_shape_custom_color = Color.RED
+			print("I see you")
+			#if aggro was kept but seen again
+			if AggroTimer.time_left != 0.0:
+				AggroTimer.stop()
+			_GoTowardsPlayer()
+		#if not colliding with player but still searching
+		elif collider.name != "Player" and PlayerRef != null:
+			if AggroTimer.time_left != 0.0:
+				pass
+			else:
+				#if no aggro yet, start timer and save last position
+				print("I lost you")
+				#start/restart timer for aggro
+				AggroTimer.start()
+				_GoTowardsPlayer()
+			RayCast.debug_shape_custom_color = Color.GREEN
+
+func _GoTowardsPlayer():
+	var playerposition = PlayerRef.global_transform.origin
+	#we commence look at, but adjust it so the y-axis of the player is in line with the enemy
+	look_at(Vector3(playerposition.x, global_position.y, playerposition.z), Vector3.UP, true)
+	target_position(playerposition)
+	
 
 func _on_aggro_timer_timeout() -> void:
 	PlayerRef = null
 	print("StopFollowing")
-	

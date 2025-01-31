084348e2a38da282b7fc1c6c9c530a3f9f139fb6
pobertt <79865756+pobertt@users.noreply.github.com>
2025-01-31 23:50:52 +0000

    interaction start

--- bigmodegamejam2025/player/player.gd
+++ bigmodegamejam2025/player/player.gd
@@ -94,6 +94,9 @@ func _input(event: InputEvent) -> void:
 		piss = false
 		p_particle.piss()
 		
+	if event.is_action_pressed("interact"):
+		interact()
+
 func _process(delta: float) -> void:
 	if piss == true:
 		bladder -= 0.0001*delta
@@ -199,3 +202,7 @@ func change_health(damage):
 	health -= damage
 	if health <= 0:
 		print("dead")
+
+func interact():
+	print("interacting")
+	Global.on_interact.emit()

c9a93066084bc514261764661dd2dad971820f40
pobertt <79865756+pobertt@users.noreply.github.com>
2025-02-01 19:32:48 +0000

    a bunch of asset stuff added and farm part of level done

--- bigmodegamejam2025/objects/test_hit.gd
+++ bigmodegamejam2025/objects/test_hit.gd
@@ -6,8 +6,8 @@ class_name DestructableObjects
 @export var audio_death : AudioStream
 @export var obj_type : ObjectType = ObjectType.NORMAL
 
-@export var explosion_force : float = 25
-@export var max_explosion_dist = 10
+@export var explosion_force : float = 10
+@export var max_explosion_dist = 15
 @export var explosion_damage: float  = 10
 
 @onready var audio_stream_player: AudioStreamPlayer3D = $AudioStreamPlayer3D
@@ -15,9 +15,13 @@ class_name DestructableObjects
 @onready var explosion_1: GPUParticles3D 
 @onready var explosion_2: GPUParticles3D
 
+@export var anim_player: AnimationPlayer
+
+
 enum ObjectType {
 	NORMAL,
 	EXPLOSIVE,
+	CAT
 }
 
 func change_health(damage):
@@ -47,13 +51,14 @@ func change_health(damage):
 func _process(delta: float) -> void:
 	if audio_stream_player.stream == audio_death && audio_stream_player.playing == false:
 		self.queue_free()
+	
 
 func push_away_objects():
 	for body in $Area3D.get_overlapping_bodies():
 		var body_pos = body.global_position
 		var force_div = 1.0
 		if body is CharacterBody3D:
-			body_pos.y += 1.0
+			body_pos.y += 4
 			force_div = 4.0 # Character bodies have no mass
 		elif body is RigidBody3D:
 			force_div = max(0.01, body.mass)
@@ -63,6 +68,7 @@ func push_away_objects():
 		if body is RigidBody3D:
 			body.apply_impulse(explosion_vec * explosion_force)
 		elif body is CharacterBody3D:
+			body.velocity = Vector3(body.velocity.x, (body.velocity.y + 1),body.velocity.z)
 			body.velocity += explosion_vec * explosion_force
 			if body.has_method("change_health"):
 				body.change_health(explosion_damage)
